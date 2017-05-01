d74337c3f1d6e4564b825aebe9d29f1989f500f4
Panachai <panachai.ny@gmail.com>
2017-05-01 20:07:21 +0700

    complete

--- TrainSolution4/Program.cs
+++ TrainSolution4/Program.cs
@@ -12,4 +12,47 @@ namespace TrainSolution4
         {
         }
     }
+
+    class Box
+    {
+        private double width, height, length, side;
+        private Box oldBox;
+        // Constructors 
+        Box(double width, double height, double length)
+        {
+            this.width = width;
+            this.height = height;
+            this.length = length;
+        }
+        Box(double side)
+        {
+            this.side = side;
+        }
+        Box(Box oldBox)
+        {
+            this.oldBox = oldBox;
+        }
+
+        // Methods 
+        double area()
+        {
+            return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
+        }
+
+        public double faceArea()
+        {
+            return width * height;
+        }
+
+        public double topArea()
+        {
+            return width * length;
+        }
+
+        public double sideArea()
+        {
+            return height * length;
+        }
+
+    }
 }
